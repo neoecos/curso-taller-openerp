b41b15b424aaff0b51b200781219028a4ab6f8b6
Sebastian Ortiz Vásquez <neoecos@gmail.com>
2013-04-05 22:58:50 -0500

    Creado valor por defecto par fecha y descripción, dejando descripción readonly en la vista.

--- lecciones/02/src/mi_modulo/mi_modulo.py
+++ lecciones/02/src/mi_modulo/mi_modulo.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
 from osv import fields, osv
 
 class mi_modulo_mi_tabla(osv.osv):
@@ -9,8 +10,21 @@ class mi_modulo_mi_tabla(osv.osv):
         'quantity': fields.integer('cantidad'),
         'date': fields.date('fecha'),
         'datetime': fields.datetime('fecha y hora'),
-        'description': fields.text('descripcion'),
-        'price': fields.float('precio', digits = (10,4)),
+        'description': fields.text('descripción'),
+        'price': fields.float('precio', digits = (10,2)),
         'state': fields.selection([('draft','borrador'),('active','Activo'),('cancelled','Cancelado')], 'estado'),
     }
+    
+    def _get_default_datetime(self, cr, uid, context = None):        
+        tenDays = timedelta(days=10)
+        tenDaysAgo = datetime.now() - tenDays
+        return tenDaysAgo.__str__()
+        #return datetime.now().__str__()
+    def _get_default_description(self, cr, uid, context = None):        
+        return "Descripción vacía por defecto"
+    _log_access = True
+    _defaults = {'datetime' : _get_default_datetime,
+                'description' : _get_default_description, }
+    
+    
 mi_modulo_mi_tabla()
